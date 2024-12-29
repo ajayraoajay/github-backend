64b8b27e0446e4344b9dfb85fadc37503c8f02a5
Ajay <ajayr2882@gmail.com>
2024-12-29 23:48:21 +0530

    final backend

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const userRoutes = require("./routes/userRoutes");
 
+
 dotenv.config(); // Load environment variables
 
 const app = express();
@@ -13,7 +14,7 @@ app.use(bodyParser.json()); // Parse JSON request bodies
 // Use user routes
 app.use("/api/users", userRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // Start the server
 app.listen(PORT, () => {
